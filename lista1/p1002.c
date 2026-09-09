bff92dadd8bb73ca02e067dea15f06562334f4c6
sddhck <caeirobuceklucas@gmail.com>
2026-09-09 14:09:42 -0300

    solucao problema 1001

---
+++ lista1/p1002.c
@@ -0,0 +1,19 @@
+//SUBMISSÃO # 48113977
+//Submissão: 11/03/2026 14:35:24
+
+#include <stdio.h>
+
+int main() {
+
+    int a, b, x;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    x = a + b;
+
+    printf("X = %d\n", x);
+
+    return 0;
+
+}
